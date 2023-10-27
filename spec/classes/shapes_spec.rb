5d82fe27a849d50151262ad7d94cc7c7c2412a5a
Chris James <quii@hey.com>
2023-10-27 11:47:01 +0100

    more shapey stuff

--- spec/classes/shapes_spec.rb
+++ spec/classes/shapes_spec.rb
@@ -14,14 +14,28 @@ class Rectangle
   end
 end
 
+class Circle
+  def initialize(radius)
+    @radius = radius
+  end
+
+  def perimeter
+    2 * Math::PI * @radius
+  end
+
+  def area
+    Math::PI * @radius ** 2
+  end
+end
+
 describe "Shapes" do
 
   matcher :have_area do |expected|
-    match { |shape| shape.area == expected}
+    match { |subject| subject.area == expected}
   end
 
   matcher :have_perimeter do |expected|
-    match { |shape| shape.perimeter == expected}
+    match { |subject| subject.perimeter == expected}
   end
 
   describe Rectangle do
@@ -29,7 +43,7 @@ describe "Shapes" do
 
     context "#perimeter" do
       it "returns the correct perimeter" do
-        expect(subject.perimeter).to eq(10)
+        expect(subject).to have_perimeter(10)
       end
     end
 
@@ -38,6 +52,21 @@ describe "Shapes" do
         expect(subject).to have_area(6)
       end
     end
+  end
 
+  describe Circle do
+    subject { Circle.new(2) }
+
+    context "#perimeter" do
+      it "returns the correct perimeter" do
+        expect(subject.perimeter).to eq(12.566370614359172)
+      end
+    end
+
+    context "#area" do
+      it "returns the correct area" do
+        expect(subject).to have_area(12.566370614359172)
+      end
+    end
   end
 end
