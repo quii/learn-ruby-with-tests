bb8df696acbf0e41601027c528dd2ad8122bd380
Chris James <quii@hey.com>
2023-11-04 14:10:45 +0000

    spacing

--- spec/greet/greet_spec.rb
+++ spec/greet/greet_spec.rb
@@ -5,6 +5,7 @@ require 'securerandom'
 shared_examples "a greeter" do
   let(:name) { SecureRandom.hex }
   let(:language) { :English }
+
   def greet
     subject.greet(name, language)
   end
@@ -17,11 +18,13 @@ shared_examples "a greeter" do
 
   context "with spanish set" do
     let(:language) { :Spanish }
+
     it "should greet the person in spanish" do
       expect(greet).to eq("Hola, #{name}!")
     end
     context "with name set to Chris" do
       let(:name) { "Chris" }
+
       it "should greet Chris, in spanish" do
         expect(greet).to eq("Hola, Chris!")
       end
@@ -30,6 +33,7 @@ shared_examples "a greeter" do
 
   context "with french set" do
     let(:language) { :French }
+
     it "should greet the person in french" do
       expect(greet).to eq("Bonjour, #{name}!")
     end
