0f0810d9334b380f19be582dae9b1c1b2a1e2c6b
Chris James <quii@hey.com>
2023-11-04 15:44:40 +0000

    very nice

--- spec/greet/greet_spec.rb
+++ spec/greet/greet_spec.rb
@@ -1,6 +1,5 @@
 require_relative '../../lib/greet/greeter.rb'
 require_relative '../../lib/greet/greeter2.rb'
-require 'securerandom'
 
 shared_context "in Spanish" do
   let(:language) { :Spanish }
@@ -14,8 +13,8 @@ shared_context "in English" do
   let(:language) { :English }
 end
 
-shared_examples "a greeter" do
-  let(:greet) { subject.greet(name, language) }
+shared_examples "a greeter" do |greeter|
+  subject { greeter.greet(name, language) }
 
   context "with no name" do
     let(:name) { nil }
@@ -23,69 +22,55 @@ shared_examples "a greeter" do
     context "with no language" do
       let(:language) { nil }
 
-      it "should greet the world in English" do
-        expect(greet).to eq("Hello, World!")
-      end
+      it {is_expected.to eq("Hello, World!")}
     end
 
     context "with the language set to Spanish" do
       include_context "in Spanish"
 
-      it "should greet the world in Spanish" do
-        expect(greet).to eq("Hola, World!")
-      end
+      it {is_expected.to eq("Hola, World!")}
     end
 
     context "with the language set to French" do
       include_context "in French"
 
-      it "should greet the world in French" do
-        expect(greet).to eq("Bonjour, World!")
-      end
+      it {is_expected.to eq("Bonjour, World!")}
     end
   end
 
   context "with a name set" do
-    let(:name) { SecureRandom.hex }
+    let(:name) { "Pepper" }
 
     context "with language set to English" do
       include_context "in English"
 
-      it "should greet the person in English" do
-        expect(greet).to eq("Hello, #{name}!")
-      end
+      it {is_expected.to eq("Hello, #{name}!")}
 
       context "with name set to Chris" do
         let(:name) { "Chris" }
 
-        it "should greet Chris in English" do
-          expect(greet).to eq("Hello, Chris!")
-        end
+        it {is_expected.to eq("Hello, Chris!")}
       end
     end
 
     context "with language set to Spanish" do
       include_context "in Spanish"
 
-      it "should greet the person in Spanish" do
-        expect(greet).to eq("Hola, #{name}!")
-      end
+      it {is_expected.to eq("Hola, #{name}!")}
     end
 
     context "with language set to French" do
       include_context "in French"
 
-      it "should greet the person in French" do
-        expect(greet).to eq("Bonjour, #{name}!")
-      end
+      it {is_expected.to eq("Bonjour, #{name}!")}
     end
   end
 end
 
 describe Greeter do
-  it_behaves_like "a greeter"
+  it_behaves_like "a greeter", Greeter.new
 end
 
 describe Greeter2 do
-  it_behaves_like "a greeter"
+  it_behaves_like "a greeter", Greeter2
 end
