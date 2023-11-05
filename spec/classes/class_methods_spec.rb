c5f3c2f5e5ef54e1b65319a85e98b547dda24df7
Chris James <quii@hey.com>
2023-11-05 09:50:30 +0000

    oopsie

--- spec/classes/class_methods_spec.rb
+++ spec/classes/class_methods_spec.rb
@@ -6,22 +6,31 @@ class Deposit
   end
 
   def Deposit.summary(*deposits)
-    deposits.reduce({}) do |summary, deposit|
-      summary[deposit.to] ||= 0
+    blah = deposits.reduce(Hash.new(0)) do |summary, deposit|
       summary[deposit.to] += deposit.amount
       summary
     end
+    blah.map { |name, amount| Balance.new(name, amount) }
   end
-
-  def self.richest_person(*deposits)
-    Deposit.summary(*deposits)
-           .max_by { |person, amount| amount }.first
-  end
-
 end
 
-describe Deposit do
-  # let deposits be an array of deposits, 3 deposits for chris, 2 for john
+class Balance
+  attr_reader :amount, :name
+  def initialize(name, amount)
+    @name = name
+    @amount = amount
+  end
+
+  def self.richest(*balance)
+    balance.max_by(&:amount).name
+  end
+
+  def ==(other)
+    other.name == name && other.amount == amount
+  end
+end
+
+describe "Deposit and balances" do
   let(:deposits) { [
     Deposit.new("Chris", 100),
     Deposit.new("Chris", 200),
@@ -31,10 +40,14 @@ describe Deposit do
   }
 
   it "can calculate the total deposits for each person" do
-    expect(Deposit.summary(*deposits)).to eq({"Chris" => 600, "John" => 300})
+    expect(Deposit.summary(*deposits)).to eq([
+      Balance.new("Chris", 600),
+      Balance.new("John", 300)
+    ])
   end
 
   it "can calculate the richest person" do
-    expect(Deposit.richest_person(*deposits)).to eq("Chris")
+    balances = Deposit.summary(*deposits)
+    expect(Balance.richest(*balances)).to eq("Chris")
   end
 end
